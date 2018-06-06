cc1b76040f55d711ca77f9e3b72634a0ad83e917
juliak1 <juliakuczynska@yahoo.com>
2018-06-06 14:38:33 +0200

    message form working

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,10 +8,10 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:error] = nil
+     
   redirect_to root_path, notice: 'Message sent successfully'
     else
-      flash.now[:error] = 'Cannot send message'
+      flash[:error] = 'Cannot send message'
       render :new
     end
   end
