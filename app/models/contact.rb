b46b3d1c63401ba9d26a72870ef993077d52971e
juliak1 <juliakuczynska@yahoo.com>
2018-06-06 14:02:47 +0200

    fix4

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -7,7 +7,7 @@ class Contact < MailForm::Base
   def headers
     {
       :subject => "Contact Form",
-      :to => "juliakuczynska@yahoo.com",
+      :to => "concertcar@yahoo.com",
       :from => %("#{name}" <#{email}>)
     }
   end
